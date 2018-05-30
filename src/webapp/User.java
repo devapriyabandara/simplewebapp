76c630428605824c860de98c11674cde0c944172
Devapriya Bandara Herath <devapriyabandara@gmail.com>
2018-05-30 10:31:06 +1000

    First commit to the project

---
+++ src/webapp/User.java
@@ -0,0 +1,11 @@
+package webapp;
+
+public class User {
+
+    public boolean isValidCredentaials(String userName, String password){
+        if (userName.equals("bandara") && password.equals("abcd"))
+            return true;
+        else
+            return false;
+    }
+}
